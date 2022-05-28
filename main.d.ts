bde683543c1df174604f1c6b8f2b96535ff3cc3f
Nguyen Van Hung <50629446+hunglannn@users.noreply.github.com>
2022-05-28 14:42:23 +0700

    Update main.d.ts

--- main.d.ts
+++ main.d.ts
@@ -9,6 +9,7 @@ export interface YouTubeProps {
   play?: boolean;
   loop?: boolean;
   fullscreen?: boolean;
+  resumePlayAndroid?:boolean;
   controls?: 1 | 2 | 3;
   showinfo?: boolean;
   modestbranding?: boolean;
